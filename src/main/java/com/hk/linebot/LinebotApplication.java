67f44294e009afbc978ca4c851490b4f8bbae9f3
yenheng <kuo330666@gmail.com>
2023-11-19 23:09:26 +0800

    [commit #1] Sprint Boot setting.

---
+++ src/main/java/com/hk/linebot/LinebotApplication.java
@@ -0,0 +1,19 @@
+package com.hk.linebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@EnableMongoAuditing
+@EntityScan("com.hk.linebot.entity")
+public class LinebotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinebotApplication.class, args);
+	}
+}
